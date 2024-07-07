63254c151bdccbcc631af90f9a9b00be698f1aea
anshukr11 <anshu@queryit.io>
2024-07-08 00:30:19 +0530

    env files added

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,10 +1,10 @@
-import mongoose, { ConnectOptions } from 'mongoose';
-
-const uri = 'mongodb+srv://anshukr96:anshusai11@cluster0.d3nmncn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+import * as dotenv from 'dotenv';
+import mongoose from 'mongoose';
+dotenv.config()
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true } as ConnectOptions);
+    await mongoose.connect(process.env.MONGODB_URI as string);
     console.log('MongoDB connected');
   } catch (err) {
     console.error(err);
